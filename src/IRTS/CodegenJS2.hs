050b4dfbe6814ca03f57ed0be93f911c019c8596
Aaron Craelius <aaroncraelius@gmail.com>
2015-12-01 23:49:43 -0500

    Attempt at converting to JS codegen

--- src/IRTS/CodegenJS2.hs
+++ src/IRTS/CodegenJS2.hs
@@ -1,57 +1,88 @@
-module IRTS.CodegenPHP(codegenPHP) where
+module IRTS.CodegenJS2(codegenJS2) where
 
 import IRTS.CodegenCommon
 import IRTS.Lang
 import IRTS.Simplified
+import IRTS.Defunctionalise
 import Idris.Core.TT
 
 import Data.Maybe
 import Data.Char
 
-codegenPHP :: CodeGenerator
-codegenPHP ci = do let out = concatMap doCodegen (simpleDecls ci)
-                   writeFile (outputFile ci) ("<?php\n" ++ helpers ++ "\n" ++
+codegenJS2 :: CodeGenerator
+codegenJS2 ci = do let out = concatMap genDDecl (defunDecls ci) ++ "\n" ++
+                             concatMap doDebug (simpleDecls ci) 
+                   writeFile (outputFile ci) (helpers ++ "\n" ++
                                                         out ++ "\n" ++ 
                                                         start ++ "\n" ++ 
                                               "\n\n")
 
-start = phpname (sMN 0 "runMain") ++ "();"
+start = js2name (sMN 0 "runMain") ++ "();"
 
 helpers = errCode ++ "\n" ++ 
           doEcho ++ "\n" ++
           doRead ++ "\n" ++
-          mkStr ++ "\n" ++
           doAppend ++ "\n"
 
-errCode = "function error($str) { echo \"$str\\n\"; exit(0); }"
-doEcho = "function idris_writeStr($str) { echo \"$str\"; }"
+errCode = "function error($str) { console.log(\"$str\\n\"); exit(0); }"
+doEcho = "function idris_writeStr($str) { console.log($str); }"
 doRead = "function idris_readStr() { return fgets(STDIN); }"
-doAppend = "function idris_append($l, $r) { return ($l . $r); }"
-mkStr = "function mkStr($l) { return array($l); }"
+doAppend = "function idris_append($l, $r) { return ($l + $r); }"
 
-phpname :: Name -> String
-phpname n = "idris_" ++ concatMap phpchar (showCG n)
-  where phpchar x | isAlpha x || isDigit x = [x]
-                  | otherwise = "_" ++ show (fromEnum x) ++ "_"
+munge :: Char -> String
+munge '<' = "LT"
+munge '>' = "GT"
+munge '=' = "EQ"
+munge '+' = "PLUS"
+munge '-' = "MINUS"
+munge '&' = "AMP"
+munge '|' = "BAR"
+munge '(' = "LPAREN"
+munge ')' = "RPAREN"
+munge '{' = "RBRACE"
+munge '}' = "RBRACE"
+munge '@' = "CIRCA"
+munge '#' = "SHARP"
+munge '*' = "STAR"
+munge x = show (fromEnum x)
+
+js2name :: Name -> String
+js2name n = concatMap js2char (showCG n)
+  where js2char x | isAlpha x || isDigit x = [x]
+                  | x == '_' = "_"
+                  | x == '.' = "$"
+                  | x == ' ' = "_"
+                  | x == ',' = "_"
+                  | otherwise = "_" ++ (munge x) ++ "_"
 
 var :: Name -> String
-var n = "$" ++ phpname n
+var n = js2name n
 
 loc :: Int -> String
-loc i = "$loc" ++ show i
+loc i = "loc" ++ show i
+
+genDDecl :: (Name, DDecl) -> String
+genDDecl (n, decl) =
+  case decl of
+    (DFun _ _ _) -> ""
+    (DConstructor name tag arity) -> "var " ++ n ++ " = \"" ++ (show t) ++ "\";\n" where n = (js2name name)
+
+
+doDebug :: (Name, SDecl) -> String
+doDebug (n, decl) =  "/*" ++ (show decl) ++ "*/\n//" ++ (show n) ++ "\n" ++ (doCodegen (n, decl))
 
 doCodegen :: (Name, SDecl) -> String
 doCodegen (n, SFun _ args i def) = cgFun n args def
 
 cgFun :: Name -> [Name] -> SExp -> String
 cgFun n args def 
-    = "function " ++ phpname n ++ "("
+    = "function " ++ js2name n ++ "("
                   ++ showSep "," (map (loc . fst) (zip [0..] args)) ++ ") {\n"
                   ++ cgBody doRet def ++ "\n}\n\n"
   where doRet :: String -> String -- Return the calculated expression
         doRet str = "return " ++ str ++ ";"
 
--- cgBody converts the SExp into a chunk of php which calculates the result
+-- cgBody converts the SExp into a chunk of js2 which calculates the result
 -- of an expression, then runs the function on the resulting bit of code.
 --
 -- We do it this way because we might calculate an expression in a deeply nested
@@ -59,9 +90,9 @@ cgFun n args def
 -- expression itself may happen quite deeply.
 
 cgBody :: (String -> String) -> SExp -> String
-cgBody ret (SV (Glob n)) = ret $ phpname n ++ "()"
+cgBody ret (SV (Glob n)) = ret $ js2name n ++ "()"
 cgBody ret (SV (Loc i)) = ret $ loc i 
-cgBody ret (SApp _ f args) = ret $ phpname f ++ "(" ++ 
+cgBody ret (SApp _ f args) = ret $ js2name f ++ "(" ++ 
                                    showSep "," (map cgVar args) ++ ")"
 cgBody ret (SLet (Loc i) v sc)
    = cgBody (\x -> loc i ++ " = " ++ x ++ ";\n") v ++
@@ -71,8 +102,8 @@ cgBody ret (SUpdate n e)
 cgBody ret (SProj e i)
    = ret $ cgVar e ++ "[" ++ show (i + 1) ++ "]"
 cgBody ret (SCon _ t n args)
-   = ret $ "array(" ++ showSep "," 
-              (show t : (map cgVar args)) ++ ")"
+   = ret $ "[" ++ showSep "," 
+              ((js2name n) : (map cgVar args)) ++ "]"
 cgBody ret (SCase _ e alts)
    = let scrvar = cgVar e 
          scr = if any conCase alts then scrvar ++ "[0]" else scrvar in
@@ -98,7 +129,7 @@ cgAlt ret scr (SConstCase t exp)
    = "case " ++ show t ++ ":\n" ++ cgBody ret exp
 cgAlt ret scr (SDefaultCase exp) = "default:\n" ++ cgBody ret exp
 cgAlt ret scr (SConCase lv t n args exp)
-   = "case " ++ show t ++ ":\n"
+   = "case " ++ js2name n ++ ":\n"
              ++ project 1 lv args ++ "\n" ++ cgBody ret exp
    where project i v [] = ""
          project i v (n : ns) = loc v ++ " = " ++ scr ++ "[" ++ show i ++ "]; "
@@ -110,7 +141,7 @@ cgVar (Glob n) = var n
 
 cgConst :: Const -> String
 cgConst (I i) = show i
-cgConst (Ch i) = show (ord i) -- Treat Char as ints, because PHP treats them as Strings...
+cgConst (Ch i) = show (ord i) -- Treat Char as ints, because JS2 treats them as Strings...
 cgConst (BI i) = show i
 cgConst (Str s) = show s
 cgConst TheWorld = "0"
@@ -136,10 +167,10 @@ cgOp (LSGe (ATInt _)) [l, r]
      = "(" ++ l ++ " >= " ++ r ++ ")"
 cgOp LStrEq [l,r] = "(" ++ l ++ " == " ++ r ++ ")"
 cgOp LStrRev [x] = "strrev(" ++ x ++ ")"
-cgOp LStrLen [x] = "strlen(utf8_decode(" ++ x ++ "))"
-cgOp LStrHead [x] = "ord(" ++ x ++ "[0])"
-cgOp LStrIndex [x, y] = "ord(" ++ x ++ "[" ++ y ++ "])"
-cgOp LStrTail [x] = "substr(" ++ x ++ ", 1)"
+cgOp LStrLen [x] = x ++ ".length"
+cgOp LStrHead [x] = x ++ "[0]"
+cgOp LStrIndex [x, y] = x ++ "[" ++ y ++ "])"
+cgOp LStrTail [x] = x ".substr(1)"
 
 cgOp (LIntStr _) [x] = "\"" ++ x ++ "\""
 cgOp (LChInt _) [x] = x
--- src/Main.hs
+++ src/Main.hs
@@ -6,22 +6,22 @@ import Idris.ElabDecls
 import Idris.REPL
 
 import IRTS.Compiler
-import IRTS.CodegenPHP
+import IRTS.CodegenJS2
 
 import System.Environment
 import System.Exit
 
-import Paths_idris_php
+import Paths_idris_js2
 
 data Opts = Opts { inputs :: [FilePath],
                    output :: FilePath }
 
-showUsage = do putStrLn "Usage: idris-php <ibc-files> [-o <output-file>]"
+showUsage = do putStrLn "Usage: idris-js2 <ibc-files> [-o <output-file>]"
                exitWith ExitSuccess
 
 getOpts :: IO Opts
 getOpts = do xs <- getArgs
-             return $ process (Opts [] "a.php") xs
+             return $ process (Opts [] "a.js2") xs
   where
     process opts ("-o":o:xs) = process (opts { output = o }) xs
     process opts ("--yes-really":xs) = process opts xs -- GRRR
@@ -32,8 +32,8 @@ c_main :: Opts -> Idris ()
 c_main opts = do elabPrims
                  loadInputs (inputs opts) Nothing
                  mainProg <- elabMain
-                 ir <- compile (Via "php") (output opts) (Just mainProg)
-                 runIO $ codegenPHP ir
+                 ir <- compile (Via "js2") (output opts) (Just mainProg)
+                 runIO $ codegenJS2 ir
 
 main :: IO ()
 main = do opts <- getOpts
